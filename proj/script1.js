0d7a6fed88100282af6f8d7c2b99ecede8e218da
Executioner501 <saidinesh0084@gmail.com>
2024-06-06 09:52:41 +0530

    tweaks

--- proj/script1.js
+++ proj/script1.js
@@ -1,3 +1,6 @@
 var username = localStorage.getItem("username");
   var dispElement = document.getElementById("disp");
   dispElement.textContent = "Welcome, " + username + "!";
+  document.querySelector('#gojo').onclick=() =>{
+    document.querySelector('.chatbot').classList.toggle('active')
+}
